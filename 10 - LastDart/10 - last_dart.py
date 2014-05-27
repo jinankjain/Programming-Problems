16d7ab64d349d0d5df62dba212fb01a029369b7e
Muhammad Hussein Nasrollahpour <iapplechocolate@me.com>
2014-05-27 08:53:05 -0700

    Update LastDart solution.

--- 10 - LastDart/10 - last_dart.py
+++ 10 - LastDart/10 - last_dart.py
@@ -2,11 +2,11 @@
 #  last_dart.py
 #  LastDart
 #
-# Created by Muhammad Hussein Nasrollahpour on 5/26/14
-# Copyright (c) 2014 Chocolate. All rights reserved.
+#  Created by Muhammad Hussein Nasrollahpour on 5/26/14
+#  Copyright (c) 2014 Chocolate. All rights reserved.
 #
 
-# input = open("./last_dart_input.txt", "r")
+input = open("./input.txt", "r")
 
 # keeps the index of each value
 index_of_each_value = []
@@ -14,34 +14,35 @@ index_of_each_value = []
 # Keeps the index of the row
 row_idx = 0
 
-for line in input:
-  values = line.split()
+with open("./output.txt", "w") as output:
+  for line in input:
+    values = line.split()
 
-  for column_idx in range(len(values)):
-    value_and_its_idx = { 'value' : values[column_idx], 'row' : row_idx, 'column' : column_idx + 1 }
+    for column_idx in range(len(values)):
+      value_and_its_idx = { 'value' : values[column_idx], 'row' : row_idx, 'column' : column_idx + 1 }
 
-    index_of_each_value.append(value_and_its_idx)
+      index_of_each_value.append(value_and_its_idx)
 
-  row_idx += 1
+    row_idx += 1
 
-values = []
-for idx in index_of_each_value:
-  values.append(int(idx['value']))
+  values = []
+  for idx in index_of_each_value:
+    values.append(int(idx['value']))
 
+  values.sort()
 
-values.sort()
+  max = values[len(values) - 1]
 
-max = values[len(values) - 1]
+  # Maximum element row index
+  max_row_idx = 0
+  # Maximum element column index
+  max_column_idx = 0
 
-# Maximum element row index
-max_row_idx = 0
-# Maximum element column index
-max_column_idx = 0
+  for idx in index_of_each_value:
+    if max == int(idx['value']):
+      max_row_idx = idx['row']
+      max_column_idx = idx['column']
 
-for idx in index_of_each_value:
-  if max == int(idx['value']):
-    max_row_idx = idx['row']
-    max_column_idx = idx['column']
+  output.write(str(max_row_idx + 1) + ' ' + str(max_column_idx))
 
-
-print max_row_idx , " ", max_column_idx
+  print max_row_idx + 1 , " ", max_column_idx
