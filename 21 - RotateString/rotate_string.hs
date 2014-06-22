0a24a12f104d8369e99f504e0fa381ed8a1b547a
Muhammad Hussein Nasrollahpour <iapplechocolate@me.com>
2014-06-22 12:52:45 -0700

    Add Author.

--- 21 - RotateString/rotate_string.hs
+++ 21 - RotateString/rotate_string.hs
@@ -1,3 +1,11 @@
+--
+-- rotate_string.hs
+-- RotateString
+--
+-- Created by Muhammad Hussein Nasrollahpour on 6/22/14
+-- Copyright (c) 2014 Chocolate. All rights reserved.
+--
+
 import Data.List
 import Control.Monad
 
@@ -6,6 +14,6 @@ rotate st = intercalate " " (tail (zipWith (++) (tails st) (inits st)))
 
 main = do
    n <- readLn :: IO Int
-   inputdata <- getContents 
-   
+   inputdata <- getContents
+
    mapM putStrLn . map rotate . lines $ inputdata
